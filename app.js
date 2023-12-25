4296802c9578a1bb283500f861eb78324e90f59d
axsrza <102383441+axsrza@users.noreply.github.com>
2023-12-25 01:40:09 -0300

    app.js

--- app.js
+++ app.js
@@ -4,44 +4,56 @@ var promiseNameInput = document.getElementById('promiseName');
 var promiseList = document.getElementById('promiseList');
 
 function addPromise() {
-    var promiseName = promiseNameInput.value;
+    if (typeof db !== 'undefined') {
+        var promiseName = promiseNameInput.value;
 
-    if (promiseName.trim() === '') {
-        alert('Por favor, insira o nome da promessa.');
-        return;
+        if (promiseName.trim() === '') {
+            alert('Por favor, insira o nome da promessa.');
+            return;
+        }
+
+        db.collection('promessas').add({
+            name: promiseName,
+            completed: false
+        });
+
+        promiseNameInput.value = '';
+    } else {
+        console.error('Firebase não inicializado corretamente.');
     }
-
-    db.collection('promessas').add({
-        name: promiseName,
-        completed: false
-    });
-
-    promiseNameInput.value = '';
 }
 
 function displayPromises() {
-    db.collection('promessas').onSnapshot(function (snapshot) {
-        promiseList.innerHTML = '';
-        snapshot.forEach(function (doc) {
-            var promiseData = doc.data();
-            var listItem = document.createElement('li');
-            listItem.textContent = promiseData.name;
+    if (typeof db !== 'undefined') {
+        db.collection('promessas').onSnapshot(function (snapshot) {
+            promiseList.innerHTML = '';
+            snapshot.forEach(function (doc) {
+                var promiseData = doc.data();
+                var listItem = document.createElement('li');
+                listItem.textContent = promiseData.name;
 
-            if (promiseData.completed) {
-                listItem.classList.add('completed');
-            }
+                if (promiseData.completed) {
+                    listItem.classList.add('completed');
+                }
 
-            listItem.addEventListener('click', function () {
-                return togglePromiseCompletion(doc.id, !promiseData.completed);
+                listItem.addEventListener('click', function () {
+                    return togglePromiseCompletion(doc.id, !promiseData.completed);
+                });
+
+                promiseList.appendChild(listItem);
             });
-
-            promiseList.appendChild(listItem);
         });
-    });
+    } else {
+        console.error('Firebase não inicializado corretamente.');
+    }
 }
 
 function togglePromiseCompletion(id, completed) {
-    db.collection('promessas').doc(id).update({ completed: completed });
+    if (typeof db !== 'undefined') {
+        db.collection('promessas').doc(id).update({ completed: completed });
+    } else {
+        console.error('Firebase não inicializado corretamente.');
+    }
 }
 
 // Verifica se o Firebase foi inicializado corretamente antes de chamar displayPromises
