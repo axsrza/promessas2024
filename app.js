4937145eb74c1c7fed4afc42742692867a773dad
axsrza <102383441+axsrza@users.noreply.github.com>
2023-12-25 01:48:32 -0300

    app.js

--- app.js
+++ app.js
@@ -5,51 +5,6 @@ var promiseNameInput = document.getElementById('promiseName');
 var promiseList = document.getElementById('promiseList');
 
 function addPromise() {
-    var promiseName = promiseNameInput.value;
-
-    if (promiseName.trim() === '') {
-        alert('Por favor, insira o nome da promessa.');
-        return;
-    }
-
-    db.collection('promessas').add({
-        name: promiseName,
-        completed: false
-    });
-
-    promiseNameInput.value = '';
-}
-
-function displayPromises() {
-    console.log(promiseList);  // Adicione esta linha
-
-    db.collection('promessas').onSnapshot(function (snapshot) {
-        promiseList.innerHTML = '';
-        snapshot.forEach(function (doc) {
-            var promiseData = doc.data();
-            var listItem = document.createElement('li');
-            listItem.textContent = promiseData.name;
-
-            if (promiseData.completed) {
-                listItem.classList.add('completed');
-            }
-
-            listItem.addEventListener('click', function () {
-                return togglePromiseCompletion(doc.id, !promiseData.completed);
-            });
-
-            promiseList.appendChild(listItem);
-        });
-    });
-}
-
-// Restante do código...
-
-var promiseForm = document.getElementById('promiseForm');
-var promiseNameInput = document.getElementById('promiseName');
-var promiseList = document.getElementById('promiseList');
-
-function addPromise() {
     if (typeof db !== 'undefined') {
         var promiseName = promiseNameInput.value;
 
