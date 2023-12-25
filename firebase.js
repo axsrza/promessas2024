562e6ecb12bd67357f44d09ba8d2d355f9280732
axsrza <102383441+axsrza@users.noreply.github.com>
2023-12-25 00:45:46 -0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 // firebase.js
 const firebaseConfig = {
-    apiKey: 'sua-chave-api',
-    authDomain: 'seu-domínio.firebaseapp.com',
-    projectId: 'seu-projeto-id',
-    storageBucket: 'seu-storage-bucket.appspot.com',
-    messagingSenderId: 'seu-sender-id',
-    appId: 'seu-app-id'
+    apiKey: 'AIzaSyA5Nuat8zdtRd8cAB64Ti-0noV6XRWBuqA',
+    authDomain: 'promessas2024.firebaseapp.com',
+    projectId: 'promessas2024',
+    storageBucket: 'promessas2024.appspot.com',
+    messagingSenderId: '1038888621959', // Substitua com o seu ID do remetente
+    appId: '1038888621959' // Substitua com o seu ID do aplicativo
 };
 
 firebase.initializeApp(firebaseConfig);
