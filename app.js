8a2dcc401f50baec0904c4806b52ee9f4fa0297e
axsrza <102383441+axsrza@users.noreply.github.com>
2023-12-25 01:19:43 -0300

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // app.js
-import { db } from './firebase';
+import { db } from './firebase.js'; // Alterei a importação para incluir a extensão ".js"
 
 const promiseForm = document.getElementById('promiseForm');
 const promiseNameInput = document.getElementById('promiseName');
