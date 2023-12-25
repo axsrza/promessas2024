8427932e21a12db1d7f749ba634268bb5966eb27
axsrza <102383441+axsrza@users.noreply.github.com>
2023-12-25 00:51:00 -0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -4,8 +4,8 @@ const firebaseConfig = {
     authDomain: 'promessas2024.firebaseapp.com',
     projectId: 'promessas2024',
     storageBucket: 'promessas2024.appspot.com',
-    messagingSenderId: '1038888621959', // Substitua com o seu ID do remetente
-    appId: '1038888621959' // Substitua com o seu ID do aplicativo
+    messagingSenderId: '1038888621959',
+    appId: '1038888621959'
 };
 
 firebase.initializeApp(firebaseConfig);
